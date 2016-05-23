e960d9e140f4ee6b721e338ac5080245737e5344
Nickolai Siskevich <n.siskevich@gmail.com>
2016-05-23 20:22:49 +0300

    Updated app init

--- app/app.ng.js
+++ app/app.ng.js
@@ -1,1 +1,23 @@
-console.log( 'Hello, cars' );
+console.log( 'Hello, Car mobile app!' );
+console.info( 'Angular version', angular.version.full );
+
+//	Angular App
+
+let App = angular.module( 'CarApp', [] );
+
+App.controller( 'MainController', [ '$scope', '$http', ( $scope, $http ) => {
+
+	$scope.hello = 'Car Mobile App!';
+	$scope.value = 'Hello, value!';
+
+	$scope.$watch( 'value', () => {
+		console.log( `Watch value: ${$scope.value}` );
+	});
+
+	$http.get( '../data/cars.json' )
+		.success( ( data ) => {
+
+console.info( data );
+
+		});
+}]);
