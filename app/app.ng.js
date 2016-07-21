1991f888f615e217ddad2df5a1a730ae90e0e635
Nickolai Siskevich <n.siskevich@gmail.com>
2016-07-21 12:44:44 +0300

    Added views, route and others update

--- app/app.ng.js
+++ app/app.ng.js
@@ -1,23 +1,74 @@
-console.log( 'Hello, Car mobile app!' );
 console.info( 'Angular version', angular.version.full );
 
 //	Angular App
 
-let App = angular.module( 'CarApp', [] );
+let App = angular.module( 'CarsApp', ['ngRoute'] );
 
-App.controller( 'MainController', [ '$scope', '$http', ( $scope, $http ) => {
+App.config([ '$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ) {
 
-	$scope.hello = 'Car Mobile App!';
-	$scope.value = 'Hello, value!';
+	// $locationProvider.html5Mode({
+	// 	enabled: true,
+	// 	requireBase: false
+	// });
 
-	$scope.$watch( 'value', () => {
-		console.log( `Watch value: ${$scope.value}` );
-	});
+	$routeProvider
+		.when('/',{
+			templateUrl: 'views/list.html',
+			controller: 'IndexCtrl'
+		})
+		.when('/list',{
+			templateUrl: 'views/list.html',
+			controller: 'IndexCtrl'
+		})
+		.when('/cars/:id',{
+			templateUrl: 'views/details.html',
+			controller: 'DetailsCtrl'
+		})
+		.otherwise({
+			retirectTo: '/'
+		});
+}]);
+
+App.controller( 'IndexCtrl', [ '$scope', '$http', '$location', function( $scope, $http, $location ) {
 
 	$http.get( '../data/cars.json' )
-		.success( ( data ) => {
+		.success(( data, status, headers, config ) => {
+			$scope.cars = data;
+			// console.log( 'status', status );
+			// console.log( 'headers', headers );
+			// console.log( 'config', config );
+			console.info( $scope.cars );
+		})
+		.error(( data, status ) => {
+			console.log("Error status : " + status);
+		});
 
-console.info( data );
+	$scope.search = '';
 
+	$scope.$watch( 'search', () => {
+		console.log( `Watch value: ${$scope.search}` );
+	});
+
+	$scope.filter = function ( item ) {
+		if ( $scope.search.length >= 2 ) {
+			return item.brand.toLowerCase() === $scope.search.toLowerCase();
+		} else {
+			return item;
+		}
+	}
+
+}]);
+
+App.controller( 'DetailsCtrl', [ '$scope', '$http', '$location', '$routeParams', function( $scope, $http, $location, $routeParams ) {
+
+	$scope.id = $routeParams.id;
+
+	$http.get( '../data/cars.json' )
+		.success(( data, status, headers, config ) => {
+			$scope.cars = data;
+			$scope.car = $scope.cars[$scope.id];
+		})
+		.error(( data, status ) => {
+			console.log("Error status : " + status);
 		});
 }]);
